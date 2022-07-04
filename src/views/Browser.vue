03be64d22306192d1ac42159488de5f39bd664fa
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2022-07-04 09:22:57 +0700

    Reworked page headers to display metadata

--- src/views/Browser.vue
+++ src/views/Browser.vue
@@ -33,10 +33,31 @@
   <i-container>
     <i-card style="transform: translateY(-50px);">
       <div class="_display:flex _justify-content:space-between">
-        <i-breadcrumb>
-          <i-breadcrumb-item>Index</i-breadcrumb-item>
-          <i-breadcrumb-item active>Collections</i-breadcrumb-item>
-        </i-breadcrumb>
+        <div class="_display:flex _justify-content:space-between _align-items:start">
+          <div>
+            <span class="_font-size:lg _font-weight:bold _margin-right:1">My Company</span> 
+            <span class="_text:muted">api.my-company.com/v3</span>
+            <p class="_font-style:italic">This is a description of my company</p>
+            <div class="_display:flex _font-size:sm">
+              <div class="_margin-right:1">
+                <img src="@/assets/icons/address-card-regular.svg" height="16" alt="">
+                contact@my-company.com
+              </div>
+              <div class="_margin-right:1">
+                <img src="@/assets/icons/handshake-regular.svg" height="16" alt="">
+                MIT license
+              </div>
+              <div class="_margin-right:1">
+                <img src="@/assets/icons/file-pdf-regular.svg" height="16" alt="">
+                Download PDF
+              </div>
+              <div>
+                <img src="@/assets/icons/file-code-regular.svg" height="16" alt="">
+                Specs Document
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="_display:flex">
           <div class="_margin-x:1" style="text-align: center;">{{ enums.length }}<br>Enums</div>
           <div class="_margin-x:1" style="text-align: center;">7<br>Schemas</div>
