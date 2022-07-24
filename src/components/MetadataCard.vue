6d14cec1f0fbeeb65f56d57f446da276cc0d5daa
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2022-07-24 17:56:18 +0700

    Updated MetadataCard component to display actual information

--- src/components/MetadataCard.vue
+++ src/components/MetadataCard.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <span class="_font-size:lg _font-weight:bold _margin-right:1">My Company</span>
-    <span class="_text:muted">api.my-company.com/v3</span>
-    <p class="_font-style:italic">This is a description of my company</p>
+    <span class="_font-size:lg _font-weight:bold _margin-right:1">
+      {{ specs.value.title }}
+    </span>
+    <span class="_text:muted">tasks.byteplug.io</span>
+    <i-button size="sm" class="_margin-left:1">
+      <img src="@/assets/icons/file-pdf-regular.svg" height="16" alt=""/>
+      <span class="_margin-left:1/2">Download PDF</span>
+    </i-button>
+    <i-button size="sm" class="_margin-left:1/2">
+      <img src="@/assets/icons/file-code-regular.svg" height="16" alt="">
+      <span class="_margin-left:1/2">Read Specs</span>
+    </i-button>
+    <p class="_font-style:italic _width:75%">{{ specs.value.summary }}</p>
     <div class="_display:flex _font-size:sm">
       <div class="_margin-right:1">
         <img src="@/assets/icons/address-card-regular.svg" height="16" alt="">
-        contact@my-company.com
+        {{ specs.value.contact.email }}
       </div>
       <div class="_margin-right:1">
         <img src="@/assets/icons/handshake-regular.svg" height="16" alt="">
-        MIT license
-      </div>
-      <div class="_margin-right:1">
-        <img src="@/assets/icons/file-pdf-regular.svg" height="16" alt="">
-        Download PDF
-      </div>
-      <div>
-        <img src="@/assets/icons/file-code-regular.svg" height="16" alt="">
-        Specs Document
+        {{ specs.value.license.name }}
       </div>
     </div>
   </div>
